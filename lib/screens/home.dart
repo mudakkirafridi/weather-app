50c621ed96a2c6023f26fa30e3d61509b9a25d2f
mudakkir <mudakkirafridi@gmail.com>
2024-11-27 08:40:08 -0800

    some improvement

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:shimmer/shimmer.dart';
 import 'package:weather_app/controller/global_controller.dart';
 import 'package:weather_app/utils/custom_colors.dart';
 import 'package:weather_app/widgets/comfort_level_widget.dart';
@@ -24,11 +25,7 @@ class _HomeScreenState extends State<HomeScreen> {
       body: SingleChildScrollView(
         child: SafeArea(
             child: Obx(() => globalController.checkLoading().isTrue
-                ? const Center(
-                    child: CircularProgressIndicator(
-                      color: Colors.black,
-                    ),
-                  )
+                ? buildShimmerEffect()
                 : SizedBox(
                     height: MediaQuery.of(context).size.height,
                     child: ListView(
@@ -54,4 +51,35 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
     );
   }
+}
+
+Widget buildShimmerEffect() {
+  return Column(
+    children: [
+      shimmerBox(height: 100, width: double.infinity), // Header shimmer
+      const SizedBox(height: 20),
+      shimmerBox(
+          height: 150, width: double.infinity), // Current weather shimmer
+      const SizedBox(height: 20),
+      shimmerBox(height: 50, width: double.infinity), // Divider shimmer
+      const SizedBox(height: 20),
+      shimmerBox(height: 200, width: double.infinity), // Comfort level shimmer
+    ],
+  );
+}
+
+Widget shimmerBox({required double height, required double width}) {
+  return Shimmer.fromColors(
+    baseColor: Colors.grey[300]!,
+    highlightColor: Colors.grey[100]!,
+    child: Container(
+      height: height,
+      width: width,
+      margin: const EdgeInsets.symmetric(horizontal: 20),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(8),
+      ),
+    ),
+  );
 }
